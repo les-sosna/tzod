51aacb53af83c338cbe080e121e8b1fccfe83ee3
p4user_0 <p4user_0@3c13c0fc-980c-11de-a05d-3bd73e269465>
2007-05-11 12:08:00 +0000

    add unfocused selection tex

--- game/workdir/scripts/textures.lua
+++ game/workdir/scripts/textures.lua
@@ -246,7 +246,8 @@ return {
      ctrl_scroll_up={left=0, right=48, top=144, bottom=156, xframes=4, yframes=1, xpivot=0, ypivot=0},
      ctrl_scroll_box={left=0, right=48, top=156, bottom=180, xframes=4, yframes=1, xpivot=0, ypivot=0},
      ctrl_scroll_down={left=0, right=48, top=180, bottom=192, xframes=4, yframes=1, xpivot=0, ypivot=0},
-     ctrl_listsel={left=1, right=31, top=193, bottom=207, xpivot=0, ypivot=0},
+     ctrl_listsel={left=2, right=30, top=194, bottom=206, xpivot=0, ypivot=0},
+     ctrl_listsel_u={left=2, right=30, top=210, bottom=222, xpivot=0, ypivot=0},
      ctrl_editcursor={left=32, right=40, top=193, bottom=207, xpivot=4, ypivot=0},     
    }
 },
