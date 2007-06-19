5eac8734868674c7fc3b19ad8b5a5db6c28cd56a
p4user_0 <p4user_0@3c13c0fc-980c-11de-a05d-3bd73e269465>
2007-06-19 14:12:00 +0000

    added skew walls

--- game/workdir/themes/winter.lua
+++ game/workdir/themes/winter.lua
@@ -14,7 +14,15 @@ return {
   wrap=true,
   content={
     brick_wall={left=0, top=0, right=128, bottom=64, xframes=4, yframes=2},
-    concrete_wall={left=0, top=80, right=128, bottom=112, xframes=4, yframes=1},
+    brick_lt={left=128, top=0, right=160, bottom=128, yframes=4},
+    brick_rt={left=160, top=0, right=192, bottom=128, yframes=4},
+    brick_rb={left=192, top=0, right=224, bottom=128, yframes=4},
+    brick_lb={left=224, top=0, right=256, bottom=128, yframes=4},
+    concrete_wall={left=0, top=64, right=128, bottom=96, xframes=4, yframes=1},
+    concrete_lt={left=0,  top=96, right=32,  bottom=128},
+    concrete_rt={left=32, top=96, right=64,  bottom=128},
+    concrete_rb={left=64, top=96, right=96,  bottom=128},
+    concrete_lb={left=96, top=96, right=128, bottom=128},
   }
 },
 {
