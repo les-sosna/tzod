6cdcf2e174c384af8df26df5e5a6e7467b058d2d
les.sosna <les.sosna@3c13c0fc-980c-11de-a05d-3bd73e269465>
2014-04-21 15:38:09 +0100

    separate ObjPtr definition

--- game/src/tank/gc/Object.h
+++ game/src/tank/gc/Object.h
@@ -4,6 +4,7 @@
 
 #include "GlobalListHelper.h"
 #include "notify.h"
+#include "ObjPtr.h"
 #include "TypeSystem.h"
 #include "core/Delegate.h"
 #include "core/SafePtr.h"
@@ -207,76 +208,6 @@ public:
 
 
 typedef void (GC_Object::*NOTIFYPROC) (GC_Object *sender, void *param);
-
-///////////////////////////////////////////////////////////////////////////////
-
-typedef void (*ObjFinalizerProc) (void *);
-
-template <class T>
-class ObjPtr
-{
-	T *_ptr;
-public:
-	ObjPtr() : _ptr(NULL) {}
-	ObjPtr(T *f)
-		: _ptr(f)
-	{
-		if( _ptr ) ++((unsigned int *) _ptr)[-1];
-	}
-	ObjPtr(const ObjPtr &f) // overwrite default copy constructor
-		: _ptr(f._ptr)
-	{
-		if( _ptr ) ++((unsigned int *) _ptr)[-1];
-	}
-
-	~ObjPtr()
-	{
-		if( _ptr && 0 == --((unsigned int *) _ptr)[-1] )
-			(*(ObjFinalizerProc*) _ptr)((unsigned int *) _ptr - 1);
-	}
-
-	const ObjPtr& operator = (T *p)
-	{
-		if( p )
-			++*((unsigned int *) p - 1);
-		if( _ptr && 0 == --((unsigned int *) _ptr)[-1] )
-			(*(ObjFinalizerProc*) _ptr)((unsigned int *) _ptr - 1);
-		_ptr = p;
-		return *this;
-	}
-
-	operator T* () const
-	{
-		return (_ptr && (((unsigned int *)_ptr)[-1] & 0x80000000)) ? _ptr : NULL;
-	}
-
-	T* operator -> () const
-	{
-		assert(*this);
-		return _ptr;
-	}
-};
-
-template<class U, class T>
-inline U* PtrDynCast(T *src)
-{
-	assert(!src || ObjPtr<T>(src));
-	return dynamic_cast<U*>(src);
-}
-
-template<class U, class T>
-inline U* PtrDynCast(const ObjPtr<T> &src)
-{
-	return dynamic_cast<U*>(src.operator T*());
-}
-
-template<class U, class T>
-inline U* PtrCast(const ObjPtr<T> &src)
-{
-	assert(!src || PtrDynCast<U>(src));
-	return static_cast<U*>(src.operator T*());
-}
-
 
 ///////////////////////////////////////////////////////////////////////////////
 class GC_Object
