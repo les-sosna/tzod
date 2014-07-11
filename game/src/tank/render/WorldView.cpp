d36554d21aa091bc9771f58278c7c320bafe3a96
les.sosna <les.sosna@3c13c0fc-980c-11de-a05d-3bd73e269465>
2014-07-11 20:49:21 +0100

    external editor-only objects rendering

--- game/src/tank/render/WorldView.cpp
+++ game/src/tank/render/WorldView.cpp
@@ -20,13 +20,23 @@
 #include "gc/RigidBody.h"
 #include "gc/Crate.h"
 #include "gc/GameClasses.h"
+#include "gc/Indicators.h"
 #include "gc/projectiles.h"
+#include "gc/Trigger.h"
 #include "gc/Turrets.h"
 #include "gc/Vehicle.h"
 #include "gc/Weapons.h"
 #include "gc/Macros.h"
 
 #include "constants.h" // TODO: ANIMATION_FPS
+
+
+const ObjectViewsSelector::ViewCollection* ObjectViewsSelector::GetViews(const GC_Actor &actor) const
+{
+	ObjectType type = actor.GetType();
+	return (type < _type2views.size() && !_type2views[type].empty()) ? &_type2views[type] : nullptr;
+}
+
 
 static bool IsWeaponAdvanced(const World &world, const GC_Actor &actor)
 {
@@ -45,88 +55,85 @@ WorldView::WorldView(IRender &render, TextureManager &tm)
     , _tm(tm)
 	, _terrain(tm)
 {
-	AddView<GC_Wall, R_Wall>(tm, "brick");
-	AddView<GC_Wall_Concrete, R_Wall>(tm, "concrete");
+	_gameViews.AddView<GC_Wall, R_Wall>(tm, "brick");
+	_gameViews.AddView<GC_Wall_Concrete, R_Wall>(tm, "concrete");
 	
-	AddView<GC_Crate, R_Sprite>(tm, "crate01", Z_WALLS);
+	_gameViews.AddView<GC_Crate, R_Sprite>(tm, "crate01", Z_WALLS);
 	
-	AddView<GC_TankBullet, R_Sprite>(tm, "projectile_cannon", Z_PROJECTILE);
-	AddView<GC_Rocket, R_Sprite>(tm, "projectile_rocket", Z_PROJECTILE);
-//	AddView<GC_Bullet, R_Sprite>(tm, "projectile_bullet", Z_PROJECTILE);
-	AddView<GC_PlazmaClod, R_Sprite>(tm, "projectile_plazma", Z_PROJECTILE);
-	AddView<GC_BfgCore, R_AnimatedSprite>(tm, "projectile_bfg", Z_PROJECTILE, ANIMATION_FPS);
-// TODO:	AddView<GC_FireSpark, xxx>(tm, "projectile_fire", Z_PROJECTILE);
-	AddView<GC_ACBullet, R_Sprite>(tm, "projectile_ac", Z_PROJECTILE);
-	AddView<GC_Disk, R_Sprite>(tm, "projectile_disk", Z_PROJECTILE);
+	_gameViews.AddView<GC_TankBullet, R_Sprite>(tm, "projectile_cannon", Z_PROJECTILE);
+	_gameViews.AddView<GC_Rocket, R_Sprite>(tm, "projectile_rocket", Z_PROJECTILE);
+//	_gameViews.AddView<GC_Bullet, R_Sprite>(tm, "projectile_bullet", Z_PROJECTILE);
+	_gameViews.AddView<GC_PlazmaClod, R_Sprite>(tm, "projectile_plazma", Z_PROJECTILE);
+	_gameViews.AddView<GC_BfgCore, R_AnimatedSprite>(tm, "projectile_bfg", Z_PROJECTILE, ANIMATION_FPS);
+// TODO:	_gameViews.AddView<GC_FireSpark, xxx>(tm, "projectile_fire", Z_PROJECTILE);
+	_gameViews.AddView<GC_ACBullet, R_Sprite>(tm, "projectile_ac", Z_PROJECTILE);
+	_gameViews.AddView<GC_Disk, R_Sprite>(tm, "projectile_disk", Z_PROJECTILE);
 	
-	AddView<GC_Spotlight, R_Sprite>(tm, "spotlight", Z_PROJECTILE);
-	AddView<GC_Light, R_Light>(tm);
+	_gameViews.AddView<GC_Spotlight, R_Sprite>(tm, "spotlight", Z_PROJECTILE);
+	_gameViews.AddView<GC_Light, R_Light>(tm);
 	
-	AddView<GC_Tank_Light, R_Vehicle>(tm);
-	AddView<GC_Tank_Light, R_HealthIndicator>(tm);
+	_gameViews.AddView<GC_Tank_Light, R_Vehicle>(tm);
+	_gameViews.AddView<GC_Tank_Light, R_HealthIndicator>(tm);
 	
-	AddView<GC_Text_ToolTip, R_Text>(tm);
+	_gameViews.AddView<GC_Text_ToolTip, R_Text>(tm);
 	
-	AddView<GC_TurretCannon, R_Turret>(tm, "turret_platform", "turret_cannon");
-	AddView<GC_TurretCannon, R_HealthIndicator>(tm);
-	AddView<GC_TurretRocket, R_Turret>(tm, "turret_platform", "turret_rocket");
-	AddView<GC_TurretRocket, R_HealthIndicator>(tm);
-	AddView<GC_TurretMinigun, R_Turret>(tm, "turret_mg_wake", "turret_mg");
-	AddView<GC_TurretMinigun, R_HealthIndicator>(tm);
-	AddView<GC_TurretGauss, R_Turret>(tm, "turret_gauss_wake", "turret_gauss");
-	AddView<GC_TurretGauss, R_HealthIndicator>(tm);
+	_gameViews.AddView<GC_TurretCannon, R_Turret>(tm, "turret_platform", "turret_cannon");
+	_gameViews.AddView<GC_TurretCannon, R_HealthIndicator>(tm);
+	_gameViews.AddView<GC_TurretRocket, R_Turret>(tm, "turret_platform", "turret_rocket");
+	_gameViews.AddView<GC_TurretRocket, R_HealthIndicator>(tm);
+	_gameViews.AddView<GC_TurretMinigun, R_Turret>(tm, "turret_mg_wake", "turret_mg");
+	_gameViews.AddView<GC_TurretMinigun, R_HealthIndicator>(tm);
+	_gameViews.AddView<GC_TurretGauss, R_Turret>(tm, "turret_gauss_wake", "turret_gauss");
+	_gameViews.AddView<GC_TurretGauss, R_HealthIndicator>(tm);
 	
-	AddView<GC_Weap_RocketLauncher, R_Weapon>(tm, "weap_ak47");
-	AddView<GC_Weap_RocketLauncher, R_Crosshair>(tm);
-	AddView<GC_Weap_RocketLauncher, R_WeapFireEffect>(tm, "particle_fire3", 0.1f, 13.0f, true);
-	AddView<GC_Weap_AutoCannon, R_Weapon>(tm, "weap_ac");
-	AddView<GC_Weap_AutoCannon, R_Crosshair>(tm);
-	AddView<GC_Weap_AutoCannon, R_Predicate<R_WeapFireEffect>>(IsWeaponAdvanced, tm, "particle_fire4", 0.135f, 17.0f, true);
-	AddView<GC_Weap_AutoCannon, R_Predicate<R_WeapFireEffect>>(IsWeaponNormal, tm, "particle_fire3", 0.135f, 17.0f, true);
-	AddView<GC_Weap_AutoCannon, R_AmmoIndicator>(tm);
-	AddView<GC_Weap_Cannon, R_Weapon>(tm, "weap_cannon");
-	AddView<GC_Weap_Cannon, R_Crosshair>(tm);
-	AddView<GC_Weap_Cannon, R_WeapFireEffect>(tm, "particle_fire3", 0.2f, 21.0f, true);
-	AddView<GC_Weap_Plazma, R_Weapon>(tm, "weap_plazma");
-	AddView<GC_Weap_Plazma, R_Crosshair>(tm);
-	AddView<GC_Weap_Plazma, R_WeapFireEffect>(tm, "particle_plazma_fire", 0.2f, 0.0f, true);
-	AddView<GC_Weap_Gauss, R_Weapon>(tm, "weap_gauss");
-	AddView<GC_Weap_Gauss, R_Crosshair>(tm);
-	AddView<GC_Weap_Gauss, R_WeapFireEffect>(tm, "particle_gaussfire", 0.15f, 0.0f, true);
-	AddView<GC_Weap_Ram, R_Weapon>(tm, "weap_ram");
-	AddView<GC_Weap_Ram, R_Crosshair>(tm);
-	AddView<GC_Weap_Ram, R_FuelIndicator>(tm);
-	AddView<GC_Weap_BFG, R_Weapon>(tm, "weap_bfg");
-	AddView<GC_Weap_BFG, R_Crosshair>(tm);
-	AddView<GC_Weap_Ripper, R_Weapon>(tm, "weap_ripper");
-	AddView<GC_Weap_Ripper, R_Crosshair>(tm);
-	AddView<GC_Weap_Ripper, R_RipperDisk>(tm);
-	AddView<GC_Weap_Minigun, R_WeaponMinigun>(tm);
-	AddView<GC_Weap_Minigun, R_Crosshair2>(tm);
-	AddView<GC_Weap_Minigun, R_Predicate<R_WeapFireEffect>>(IsWeaponAdvanced, tm, "particle_fire3", 0.1f, 17.0f, true);
-	AddView<GC_Weap_Minigun, R_WeapFireEffect>(tm, "minigun_fire", 0.1f, 20.0f, false);
-	AddView<GC_Weap_Zippo, R_Weapon>(tm, "weap_zippo");
-	AddView<GC_Weap_Zippo, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_RocketLauncher, R_Weapon>(tm, "weap_ak47");
+	_gameViews.AddView<GC_Weap_RocketLauncher, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_RocketLauncher, R_WeapFireEffect>(tm, "particle_fire3", 0.1f, 13.0f, true);
+	_gameViews.AddView<GC_Weap_AutoCannon, R_Weapon>(tm, "weap_ac");
+	_gameViews.AddView<GC_Weap_AutoCannon, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_AutoCannon, R_Predicate<R_WeapFireEffect>>(IsWeaponAdvanced, tm, "particle_fire4", 0.135f, 17.0f, true);
+	_gameViews.AddView<GC_Weap_AutoCannon, R_Predicate<R_WeapFireEffect>>(IsWeaponNormal, tm, "particle_fire3", 0.135f, 17.0f, true);
+	_gameViews.AddView<GC_Weap_AutoCannon, R_AmmoIndicator>(tm);
+	_gameViews.AddView<GC_Weap_Cannon, R_Weapon>(tm, "weap_cannon");
+	_gameViews.AddView<GC_Weap_Cannon, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Cannon, R_WeapFireEffect>(tm, "particle_fire3", 0.2f, 21.0f, true);
+	_gameViews.AddView<GC_Weap_Plazma, R_Weapon>(tm, "weap_plazma");
+	_gameViews.AddView<GC_Weap_Plazma, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Plazma, R_WeapFireEffect>(tm, "particle_plazma_fire", 0.2f, 0.0f, true);
+	_gameViews.AddView<GC_Weap_Gauss, R_Weapon>(tm, "weap_gauss");
+	_gameViews.AddView<GC_Weap_Gauss, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Gauss, R_WeapFireEffect>(tm, "particle_gaussfire", 0.15f, 0.0f, true);
+	_gameViews.AddView<GC_Weap_Ram, R_Weapon>(tm, "weap_ram");
+	_gameViews.AddView<GC_Weap_Ram, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Ram, R_FuelIndicator>(tm);
+	_gameViews.AddView<GC_Weap_BFG, R_Weapon>(tm, "weap_bfg");
+	_gameViews.AddView<GC_Weap_BFG, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Ripper, R_Weapon>(tm, "weap_ripper");
+	_gameViews.AddView<GC_Weap_Ripper, R_Crosshair>(tm);
+	_gameViews.AddView<GC_Weap_Ripper, R_RipperDisk>(tm);
+	_gameViews.AddView<GC_Weap_Minigun, R_WeaponMinigun>(tm);
+	_gameViews.AddView<GC_Weap_Minigun, R_Crosshair2>(tm);
+	_gameViews.AddView<GC_Weap_Minigun, R_Predicate<R_WeapFireEffect>>(IsWeaponAdvanced, tm, "particle_fire3", 0.1f, 17.0f, true);
+	_gameViews.AddView<GC_Weap_Minigun, R_WeapFireEffect>(tm, "minigun_fire", 0.1f, 20.0f, false);
+	_gameViews.AddView<GC_Weap_Zippo, R_Weapon>(tm, "weap_zippo");
+	_gameViews.AddView<GC_Weap_Zippo, R_Crosshair>(tm);
 
-//	AddView<GC_pu_Health, R_AnimatedSprite>(tm, "pu_health", Z_FREE_ITEM, ANIMATION_FPS);
-	AddView<GC_pu_Mine, R_Sprite>(tm, "item_mine", Z_FREE_ITEM);
-//	AddView<GC_pu_Shield, xxx>(tm);
-//	AddView<GC_pu_Shock, xxx>(tm);
-//	AddView<GC_pu_Booster, xxx>(tm);
-
+//	_gameViews.AddView<GC_pu_Health, R_AnimatedSprite>(tm, "pu_health", Z_FREE_ITEM, ANIMATION_FPS);
+	_gameViews.AddView<GC_pu_Mine, R_Sprite>(tm, "item_mine", Z_FREE_ITEM);
+//	_gameViews.AddView<GC_pu_Shield, xxx>(tm);
+//	_gameViews.AddView<GC_pu_Shock, xxx>(tm);
+//	_gameViews.AddView<GC_pu_Booster, xxx>(tm);
 	
-	AddView<GC_Wood, R_Tile>(tm, "wood", Z_WOOD);
-	AddView<GC_Water, R_Tile>(tm, "water", Z_WATER);
+	_gameViews.AddView<GC_Wood, R_Tile>(tm, "wood", Z_WOOD);
+	_gameViews.AddView<GC_Water, R_Tile>(tm, "water", Z_WATER);
+
+	_editorViews.AddView<GC_HideLabel, R_Sprite>(tm, "editor_item", Z_EDITOR);
+	_editorViews.AddView<GC_SpawnPoint, R_Sprite>(tm, "editor_respawn", Z_EDITOR);
+	_editorViews.AddView<GC_Trigger, R_Sprite>(tm, "editor_trigger", Z_WOOD);
 }
 
 WorldView::~WorldView()
 {
-}
-
-const WorldView::ViewCollection* WorldView::GetViews(const GC_Actor &actor) const
-{
-	ObjectType type = actor.GetType();
-	return (type < _type2views.size() && !_type2views[type].empty()) ? &_type2views[type] : nullptr;
 }
 
 void WorldView::Render(World &world, const FRECT &view, bool editorMode) const
@@ -180,7 +187,7 @@ void WorldView::Render(World &world, const FRECT &view, bool editorMode) const
     {
         FOREACH(world.grid_sprites.element(x,y), GC_2dSprite, object)
         {
-			if( const ViewCollection *viewCollection = GetViews(*object) )
+			if( auto *viewCollection = GetViews(*object, editorMode) )
 			{
 				for( auto &view: *viewCollection )
 				{
@@ -200,7 +207,7 @@ void WorldView::Render(World &world, const FRECT &view, bool editorMode) const
 
     FOREACH( world.GetList(LIST_gsprites), GC_2dSprite, object )
     {
-		if( const ViewCollection *viewCollection = GetViews(*object) )
+		if( auto *viewCollection = GetViews(*object, editorMode) )
 		{
 			for( auto &view: *viewCollection )
 			{
@@ -230,4 +237,13 @@ void WorldView::Render(World &world, const FRECT &view, bool editorMode) const
 		}
         zLayers[z].clear();
     }
+}
+
+inline const ObjectViewsSelector::ViewCollection* WorldView::GetViews(const GC_Actor &actor, bool editorMode) const
+{
+	// In editor mode give priority to _editorViews
+	const ObjectViewsSelector::ViewCollection *viewCollection;
+	viewCollection = editorMode ? _editorViews.GetViews(actor) : nullptr;
+	viewCollection = viewCollection ? viewCollection : _gameViews.GetViews(actor);
+	return viewCollection;
 }
--- game/src/tank/render/WorldView.h
+++ game/src/tank/render/WorldView.h
@@ -12,6 +12,23 @@ struct FRECT;
 struct IRender;
 struct ObjectView;
 
+class ObjectViewsSelector
+{
+public:
+	typedef std::vector<std::unique_ptr<ObjectView>> ViewCollection;
+	template <class T, class View, class ...Args>
+	void AddView(Args && ...args)
+	{
+		auto type = T::GetTypeStatic();
+		if (_type2views.size() <= type)
+			_type2views.resize(type + 1);
+		_type2views[type].emplace_back(new View(std::forward<Args>(args)...));
+	}
+	const ViewCollection* GetViews(const GC_Actor &actor) const;
+private:
+	std::vector<ViewCollection> _type2views;
+};
+
 class WorldView
 {
 public:
@@ -23,16 +40,7 @@ private:
     IRender &_render;
     TextureManager &_tm;
 	Terrain _terrain;
-	
-	typedef std::vector<std::unique_ptr<ObjectView>> ViewCollection;
-	std::vector<ViewCollection> _type2views;
-	template <class T, class View, class ...Args>
-	void AddView(Args && ...args)
-	{
-		auto type = T::GetTypeStatic();
-		if (_type2views.size() <= type)
-			_type2views.resize(type + 1);
-		_type2views[type].emplace_back(new View(std::forward<Args>(args)...));
-	}
-	const ViewCollection* GetViews(const GC_Actor &actor) const;
+	ObjectViewsSelector _gameViews;
+	ObjectViewsSelector _editorViews;
+	inline const ObjectViewsSelector::ViewCollection* GetViews(const GC_Actor &actor, bool editorMode) const;
 };
