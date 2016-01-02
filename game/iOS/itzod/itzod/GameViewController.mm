a4e8c4705f9625b51614353ecf4186512dc05754
les.sosna <les.sosna@3c13c0fc-980c-11de-a05d-3bd73e269465>
2016-01-02 11:36:25 -0800

    do not crash on context creation failure

--- game/iOS/itzod/itzod/GameViewController.mm
+++ game/iOS/itzod/itzod/GameViewController.mm
@@ -15,7 +15,6 @@
 @property (strong, nonatomic) EAGLContext *context;
 @property (strong, nonatomic) GLKBaseEffect *effect;
 
-- (void)setupGL;
 - (void)tearDownGL;
 
 @end
@@ -29,25 +28,27 @@
     self.preferredFramesPerSecond = 60;
     self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
 
-    if (!self.context) {
+    if (!self.context)
+    {
         NSLog(@"Failed to create ES context");
     }
-    
-    GLKView *view = (GLKView *)self.view;
-    view.context = self.context;
-    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
-    
-    [self setupGL];
+    else
+    {
+        GLKView *view = (GLKView *)self.view;
+        view.context = self.context;
+        view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+        
+        [EAGLContext setCurrentContext:self.context];
+
+        AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+        _appWindow.reset(new CocoaTouchWindow());
+        _tzodView.reset(new TzodView(appDelegate.fs, appDelegate.logger, appDelegate.app, *_appWindow));
+    }
 }
 
 - (void)dealloc
 {    
     [self tearDownGL];
-    
-    if ([EAGLContext currentContext] == self.context)
-    {
-        [EAGLContext setCurrentContext:nil];
-    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -57,14 +58,7 @@
     if ([self isViewLoaded] && ([[self view] window] == nil))
     {
         self.view = nil;
-        
         [self tearDownGL];
-        
-        if ([EAGLContext currentContext] == self.context)
-        {
-            [EAGLContext setCurrentContext:nil];
-        }
-        self.context = nil;
     }
 
     // Dispose of any resources that can be recreated.
@@ -75,20 +69,15 @@
     return YES;
 }
 
-- (void)setupGL
-{
-    [EAGLContext setCurrentContext:self.context];
-    
-    AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    _appWindow.reset(new CocoaTouchWindow());
-    _tzodView.reset(new TzodView(appDelegate.fs, appDelegate.logger, appDelegate.app, *_appWindow));
-}
-
 - (void)tearDownGL
 {
     _tzodView.reset();
     _appWindow.reset();
-    [EAGLContext setCurrentContext:self.context];
+    if ([EAGLContext currentContext] == self.context)
+    {
+        [EAGLContext setCurrentContext:nil];
+    }
+    self.context = nil;
 }
 
 #pragma mark - GLKView and GLKViewController delegate methods
