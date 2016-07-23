4d75ad7adfe2f710c9f62cce1f9df3cb9dfcf3c8
les.sosna <les.sosna@3c13c0fc-980c-11de-a05d-3bd73e269465>
2016-07-23 03:00:28 -0700

    move ctor parts to initializer list

--- game/src/gc/World.cpp
+++ game/src/gc/World.cpp
@@ -12,16 +12,16 @@
 #include <fs/FileSystem.h>
 #include <MapFile.h>
 
-
-// don't create game objects in the constructor
 World::World(int X, int Y)
   : _gameStarted(false)
   , _frozen(false)
   , _nightMode(false)
-  , _sx(0)
-  , _sy(0)
-  , _locationsX(0)
-  , _locationsY(0)
+  , _sx((float)X * CELL_SIZE)
+  , _sy((float)Y * CELL_SIZE)
+  , _cellsX(X)
+  , _cellsY(Y)
+  , _locationsX((X * CELL_SIZE + LOCATION_SIZE - 1) / LOCATION_SIZE)
+  , _locationsY((Y * CELL_SIZE + LOCATION_SIZE - 1) / LOCATION_SIZE)
   , _seed(1)
   , _safeMode(true)
   , _time(0)
@@ -31,12 +31,7 @@ World::World(int X, int Y)
   , _dump(nullptr)
 #endif
 {
-	_locationsX  = (X * CELL_SIZE / LOCATION_SIZE + ((X * CELL_SIZE) % LOCATION_SIZE != 0 ? 1 : 0));
-	_locationsY  = (Y * CELL_SIZE / LOCATION_SIZE + ((Y * CELL_SIZE) % LOCATION_SIZE != 0 ? 1 : 0));
-	_sx          = (float) X * CELL_SIZE;
-	_sy          = (float) Y * CELL_SIZE;
-	_cellsX      = X;
-	_cellsY      = Y;
+	// don't create game objects in the constructor
 
 	grid_rigid_s.resize(_locationsX, _locationsY);
 	grid_walls.resize(_locationsX, _locationsY);
