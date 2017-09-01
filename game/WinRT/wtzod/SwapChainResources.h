2e020d60ffc5de48db1afebd81494b3595582f09
Andrei Aristarkhov <les.sosna@gmail.com>
2017-09-01 15:59:55 -0700

    disable warnings for oggvorbis

--- game/WinRT/wtzod/SwapChainResources.h
+++ game/WinRT/wtzod/SwapChainResources.h
@@ -14,20 +14,20 @@ namespace DX
 		bool SetCurrentOrientation(Windows::Graphics::Display::DisplayOrientations currentOrientation);
 		bool SetDpi(float dpi);
 
-		IDXGISwapChain1*		GetSwapChain() const { return m_swapChain.Get(); }
-		ID3D11RenderTargetView*	GetBackBufferRenderTargetView() const { return m_d3dRenderTargetView.Get(); }
+		IDXGISwapChain1* GetSwapChain() const { return m_swapChain.Get(); }
+		ID3D11RenderTargetView* GetBackBufferRenderTargetView() const { return m_d3dRenderTargetView.Get(); }
 
 	private:
 		bool ResizeSwapChain(Windows::Foundation::Size logicalSize, float dpi);
 
 		DeviceResources &m_deviceResources;
 
-		Microsoft::WRL::ComPtr<IDXGISwapChain1>			m_swapChain;
-		Microsoft::WRL::ComPtr<ID3D11RenderTargetView>	m_d3dRenderTargetView;
+		Microsoft::WRL::ComPtr<IDXGISwapChain1> m_swapChain;
+		Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_d3dRenderTargetView;
 
-		Windows::Foundation::Size						m_logicalSize;
-		Windows::Graphics::Display::DisplayOrientations	m_nativeOrientation;
-		Windows::Graphics::Display::DisplayOrientations	m_currentOrientation;
-		float											m_dpi;
+		Windows::Foundation::Size m_logicalSize;
+		Windows::Graphics::Display::DisplayOrientations m_nativeOrientation;
+		Windows::Graphics::Display::DisplayOrientations m_currentOrientation;
+		float m_dpi;
 	};
 }
