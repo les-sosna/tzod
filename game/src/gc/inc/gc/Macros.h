ee67c39e1d269d7c73c44aef65913838879f0436
Andrei Aristarkhov <les.sosna@gmail.com>
2017-09-05 21:32:07 -0700

    fix warnings

--- game/src/gc/inc/gc/Macros.h
+++ game/src/gc/inc/gc/Macros.h
@@ -3,8 +3,8 @@
 #define SAFE_KILL(w, p)      { if(p) (p)->Kill(w); (p)=nullptr; }
 
 #define FOREACH(ls, type, var)                                              \
-	if( type *var = (type *) (0xffffffff) )                                 \
-	for( PtrList<GC_Object>::id_type var##iterator = (ls).begin();          \
-	         var##iterator != (ls).end() &&                                 \
+    if( type *var = (type *) (-1) )                                         \
+    for( PtrList<GC_Object>::id_type var##iterator = (ls).begin();          \
+             var##iterator != (ls).end() &&                                 \
              ((var = static_cast<type *>((ls).at(var##iterator))), true);   \
-	    var##iterator = (ls).next(var##iterator))
+        var##iterator = (ls).next(var##iterator))
