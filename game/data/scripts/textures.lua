8cd768ee10db68993f1b8f4ac081fc3aaa57b42c
Nadia <neetoot@gmail.com>
2018-12-16 14:46:11 -0800

    remove editor item label; improve respawn point appearance

--- game/data/scripts/textures.lua
+++ game/data/scripts/textures.lua
@@ -31,7 +31,7 @@ return {
 {
    file="textures/editor/labels.tga",
    content={
-     editor_respawn={left=0, top=0, right=32, bottom=32},
+     editor_respawn={left=0, top=0, right=64, bottom=64},
      editor_item={left=64, top=0, right=128, bottom=64},
    }
 },
