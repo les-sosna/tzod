038402c64b1af884b63436af04815aca25f2a79b
Andrei Aristarkhov <les.sosna@gmail.com>
2019-01-06 21:39:17 -0800

    crate is an obstacle

--- game/src/gc/inc/gc/Crate.h
+++ game/src/gc/inc/gc/Crate.h
@@ -13,5 +13,5 @@ public:
 	void OnDestroy(World &world, const DamageDesc &dd) override;
 
 	float GetDefaultHealth() const override { return 50; }
-	uint8_t GetObstacleFlags() const override { return 0; }
+	uint8_t GetObstacleFlags() const override { return 1; }
 };
