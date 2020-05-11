80b827ec3227f0aa50685c65991b16f968f1cfe0
Andrei Aristarkhov <les.sosna@gmail.com>
2020-05-10 23:29:26 -0700

    cleanup

--- game/as/inc/as/AppConstants.h
+++ game/as/inc/as/AppConstants.h
@@ -1,13 +1,6 @@
 #pragma once
 
-#define DIR_SCRIPTS      "scripts"
-#define DIR_SAVE         "save"
 #define DIR_MAPS         "maps"
 #define DIR_SPRITES      "sprites"
-#define DIR_THEMES       "themes"
 #define DIR_MUSIC        "music"
 #define DIR_SOUND        "sounds"
-#define DIR_SCREENSHOTS  "screenshots"
-
-#define FILE_TEXTURES    DIR_SCRIPTS"/textures.lua"
-#define FILE_STARTUP     DIR_SCRIPTS"/init.lua"
